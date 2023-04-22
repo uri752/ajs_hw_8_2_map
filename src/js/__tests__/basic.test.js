54e5dc572637ff27ea15c02ca6229c48e82e432b
uri752 <uri752blag@mail.ru>
2023-04-22 08:40:43 +0300

    fix error lint

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,18 +1,18 @@
-/*import sum from '../basic';
+/* import sum from '../basic';
 
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
-});*/
+}); */
 
-import ErrorRepository from "../app";
+import ErrorRepository from '../app';
 
 test('error exists', () => {
   const rep = new ErrorRepository();
   const code52 = 52;
   const text52 = 'Ошибка 52';
-  rep.errors.set(code52, text52) 
+  rep.errors.set(code52, text52);
   expect(rep.translate(code52)).toBe(text52);
 });
 
@@ -22,6 +22,6 @@ test('erorr doesnt exist', () => {
   const text52 = 'Ошибка 52';
   const code100 = 100;
   const textUnknownError = 'Unknown error';
-  rep.errors.set(code52, text52) 
+  rep.errors.set(code52, text52);
   expect(rep.translate(code100)).toBe(textUnknownError);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,22 @@
-/*// TODO: write your code here
+/* // TODO: write your code here
 import sum from './basic';
 
 console.log('worked');
 
-console.log(sum([1, 2]));*/
+console.log(sum([1, 2])); */
 
 export default class ErrorRepository {
-    constructor() {
-        this.errors = new Map(); //ключ - это код ошибки (число), а значение - текстовое описание (человекочитаемое).
-    }
+  constructor() {
+    this.errors = new Map();
+    // ключ - это код ошибки (число),
+    // а значение - текстовое описание (человекочитаемое).
+  }
 
-    translate(code) {
-        let text = this.errors.get(code);
-        if (text == undefined) {
-            text = 'Unknown error';
-        }
-        return text;
+  translate(code) {
+    let text = this.errors.get(code);
+    if (text === undefined) {
+      text = 'Unknown error';
     }
+    return text;
+  }
 }
